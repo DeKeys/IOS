86ab943d1dddf39bf327f837dab911cb65744d6a
SkullMag <rybalko.oleg.123@mail.ru>
2022-07-09 21:49:03 +0300

    feat: added function for retrieving passwords from core data

--- PasswordsApp/Common/Service/CoreDataLayer.swift
+++ PasswordsApp/Common/Service/CoreDataLayer.swift
@@ -14,6 +14,10 @@ typealias CachedPasswords = [CachePassword]
 struct CoreDataWorker {
     var context: NSManagedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
+    /// Function for adding password to the core data
+    ///
+    /// - Parameter password: password entity
+    /// - Throws: NSManagedObjectContext.save error
     func addPassword(password: Password) throws {
         let newPassword = CachePassword(context: context)
         newPassword.createdAt = Int32(password.createdAt)
@@ -24,5 +28,49 @@ struct CoreDataWorker {
         
         context.insert(newPassword)
         try context.save()
+    }
+    
+    /// Function for retrieving passwords from core data
+    ///
+    /// - Throws: NSManagedObjectContext.fetch error
+    func fetchPasswords() throws -> CachedPasswords {
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "CachePassword")
+        let passwords = try context.fetch(request) as! CachedPasswords
+        return passwords
+    }
+    
+    /// Function for retrieving one password from core data
+    ///
+    /// - Returns: found password or nil
+    func fetchPassword(password: Password, fields: [Password.CodingKeys]?) -> CachePassword? {
+        let request: NSFetchRequest<CachePassword>
+        request = CachePassword.fetchRequest()
+        
+        var predicates = [NSPredicate]()
+        
+        if let fields = fields {
+            for f in fields {
+                switch f {
+                case .login:
+                    predicates.append(NSPredicate(format: "login = %@", password.login))
+                case .password:
+                    predicates.append(NSPredicate(format: "password = %@", password.password))
+                case .serviceName:
+                    predicates.append(NSPredicate(format: "serviceName = %@", password.serviceName))
+                case .ipfsHash:
+                    predicates.append(NSPredicate(format: "ipfsHash = %@", password.ipfsHash))
+                case .createdAt:
+                    predicates.append(NSPredicate(format: "createdAt = %@", password.createdAt))
+                }
+            }
+        }
+        
+        request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: predicates)
+        
+        do {
+            return try context.fetch(request).first
+        } catch {
+            return nil
+        }
     }
 }
