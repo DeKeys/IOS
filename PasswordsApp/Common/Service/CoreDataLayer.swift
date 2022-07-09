345f1cbd95369eb40974789fc7e6e1e41ba011ed
SkullMag <rybalko.oleg.123@mail.ru>
2022-07-09 23:20:59 +0300

    feat: changed fields in core data

--- PasswordsApp/Common/Service/CoreDataLayer.swift
+++ PasswordsApp/Common/Service/CoreDataLayer.swift
@@ -9,8 +9,6 @@ import Foundation
 import CoreData
 import UIKit
 
-typealias CachedPasswords = [CachePassword]
-
 struct CoreDataWorker {
     var context: NSManagedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
@@ -18,13 +16,10 @@ struct CoreDataWorker {
     ///
     /// - Parameter password: password entity
     /// - Throws: NSManagedObjectContext.save error
-    func addPassword(password: Password) throws {
+    func addPassword(ipfsHash: String) throws {
         let newPassword = CachePassword(context: context)
-        newPassword.createdAt = Int32(password.createdAt)
-        newPassword.password = password.password
-        newPassword.ipfsHash = password.ipfsHash
-        newPassword.serviceName = password.serviceName
-        newPassword.login = password.login
+        newPassword.ipfsHash = ipfsHash
+        newPassword.pinIndex = -1
         
         context.insert(newPassword)
         try context.save()
@@ -33,44 +28,8 @@ struct CoreDataWorker {
     /// Function for retrieving passwords from core data
     ///
     /// - Throws: NSManagedObjectContext.fetch error
-    func fetchPasswords() throws -> CachedPasswords {
-        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "CachePassword")
-        let passwords = try context.fetch(request) as! CachedPasswords
-        return passwords
-    }
-    
-    /// Function for retrieving one password from core data
-    ///
-    /// - Returns: found password or nil
-    func fetchPassword(password: Password, fields: [Password.CodingKeys]?) -> CachePassword? {
-        let request: NSFetchRequest<CachePassword>
-        request = CachePassword.fetchRequest()
-        
-        var predicates = [NSPredicate]()
-        
-        if let fields = fields {
-            for f in fields {
-                switch f {
-                case .login:
-                    predicates.append(NSPredicate(format: "login = %@", password.login))
-                case .password:
-                    predicates.append(NSPredicate(format: "password = %@", password.password))
-                case .serviceName:
-                    predicates.append(NSPredicate(format: "serviceName = %@", password.serviceName))
-                case .ipfsHash:
-                    predicates.append(NSPredicate(format: "ipfsHash = %@", password.ipfsHash))
-                case .createdAt:
-                    predicates.append(NSPredicate(format: "createdAt = %@", password.createdAt))
-                }
-            }
-        }
-        
-        request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: predicates)
-        
-        do {
-            return try context.fetch(request).first
-        } catch {
-            return nil
-        }
+    /// - Returns: array of passwords from core data
+    func fetchPasswords() throws -> [CachePassword] {
+        return try context.fetch(CachePassword.fetchRequest())
     }
 }
