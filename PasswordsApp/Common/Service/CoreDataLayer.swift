dbe014fab88387a2efd1611eb82509854f8cd51d
SkullMag <rybalko.oleg.123@mail.ru>
2022-07-09 21:13:02 +0300

    feat: function for adding password to core data

--- PasswordsApp/Common/Service/CoreDataLayer.swift
+++ PasswordsApp/Common/Service/CoreDataLayer.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import CoreData
+import UIKit
+
+typealias CachedPasswords = [CachePassword]
+
+struct CoreDataWorker {
+    var context: NSManagedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    func addPassword(password: Password) throws {
+        let newPassword = CachePassword(context: context)
+        newPassword.createdAt = Int32(password.createdAt)
+        newPassword.password = password.password
+        newPassword.ipfsHash = password.ipfsHash
+        newPassword.serviceName = password.serviceName
+        newPassword.login = password.login
+        
+        context.insert(newPassword)
+        try context.save()
+    }
+}
