de0df508928e1ce9d203c64c74e2728af0c96138
SkullMag <rybalko.oleg.123@mail.ru>
2022-07-16 01:04:49 +0300

    feat(NewPassword): created UI for adding passwords

--- PasswordsApp/Module/NewPassword/NewPasswordViewController.swift
+++ PasswordsApp/Module/NewPassword/NewPasswordViewController.swift
@@ -6,3 +6,139 @@
 //
 
 import Foundation
+import UIKit
+
+class NewPasswordViewController: UIViewController {
+    
+    var presenter: NewPasswordPresenterProtocol?
+    
+    var serviceStackView = UIStackView()
+    var loginStackView = UIStackView()
+    var passwordStackView = UIStackView()
+    var passwordTextFieldStackView = UIStackView()
+    
+    var serviceLabel = UILabel()
+    var loginLabel = UILabel()
+    var passwordLabel = UILabel()
+    
+    var serviceTextField = EntryTextField()
+    var loginTextField = EntryTextField()
+    var passwordTextField = PasswordTextField()
+    
+    var generatePasswordButton = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+        setupNavigation()
+        setupConstraints()
+    }
+    
+    private func setupUI() {
+        self.view.backgroundColor = .viewBackground
+        
+        // Setup service name stack
+        self.serviceLabel.text = "Service name:"
+        self.serviceLabel.font = .systemFont(ofSize: 16, weight: .semibold)
+        
+        
+        self.serviceTextField.placeholder = "Google"
+        
+        self.serviceStackView.axis = .vertical
+        self.serviceStackView.spacing = 8
+        
+        self.serviceStackView.addArrangedSubview(self.serviceLabel)
+        self.serviceStackView.addArrangedSubview(self.serviceTextField)
+        
+        // Setup login stack
+        self.loginLabel.text = "Login:"
+        self.loginLabel.font = .systemFont(ofSize: 16, weight: .semibold)
+        
+        self.loginTextField.placeholder = "test"
+        self.loginTextField.textContentType = .username
+        
+        self.loginStackView.axis = .vertical
+        self.loginStackView.spacing = 8
+        
+        self.loginStackView.addArrangedSubview(self.loginLabel)
+        self.loginStackView.addArrangedSubview(self.loginTextField)
+        
+        var generateButtonConfig = UIButton.Configuration.plain()
+        generateButtonConfig.image = UIImage(systemName: "lock.shield")
+        
+        // Setup generate button
+        self.generatePasswordButton.clipsToBounds = true
+        self.generatePasswordButton.configuration = generateButtonConfig
+        self.generatePasswordButton.backgroundColor = .black
+        self.generatePasswordButton.tintColor = .white
+        self.generatePasswordButton.layer.cornerRadius = 10
+        self.generatePasswordButton.addTarget(self, action: #selector(generateButtonTapped), for: .touchDown)
+        
+        // Setup password stack
+        self.passwordLabel.text = "Password:"
+        self.passwordLabel.font = .systemFont(ofSize: 16, weight: .semibold)
+        
+        self.passwordTextField.placeholder = "qwerty"
+        
+        self.passwordStackView.axis = .vertical
+        self.passwordStackView.spacing = 8
+        
+        self.passwordTextFieldStackView.axis = .horizontal
+        self.passwordTextFieldStackView.alignment = .trailing
+        self.passwordTextFieldStackView.distribution = .fillProportionally
+        self.passwordTextFieldStackView.addArrangedSubview(self.passwordTextField)
+        self.passwordTextFieldStackView.addArrangedSubview(self.generatePasswordButton)
+        
+        self.passwordStackView.addArrangedSubview(self.passwordLabel)
+        self.passwordStackView.addArrangedSubview(self.passwordTextFieldStackView)
+        
+        self.view.addSubview(serviceStackView)
+        self.view.addSubview(loginStackView)
+        self.view.addSubview(passwordStackView)
+    }
+    
+    private func setupNavigation() {
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.title = "Create password"
+        
+        let createButton = UIBarButtonItem(title: "Create", style: .done, target: self, action: #selector(createButtonTapped))
+        createButton.tintColor = .textColor
+        
+        self.navigationItem.rightBarButtonItem = createButton
+    }
+    
+    @objc func createButtonTapped(_ sender: UIBarButtonItem?) {
+        print("Create pwd")
+    }
+    
+    @objc func generateButtonTapped(_ sender: UIButton?) {
+        print("Generate password")
+    }
+
+    private func setupConstraints() {
+        serviceStackView.snp.makeConstraints { make in
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(20)
+            make.left.equalTo(12)
+            make.right.equalTo(-12)
+        }
+        
+        loginStackView.snp.makeConstraints { make in
+            make.top.equalTo(self.serviceStackView.snp.bottom).offset(20)
+            make.left.equalTo(12)
+            make.right.equalTo(-12)
+        }
+        
+        passwordStackView.snp.makeConstraints { make in
+            make.top.equalTo(self.loginStackView.snp.bottom).offset(20)
+            make.left.equalTo(12)
+            make.right.equalTo(-12)
+        }
+        
+        generatePasswordButton.snp.makeConstraints { make in
+            make.width.equalTo(50)
+            make.height.equalTo(self.passwordTextField.snp.height)
+            make.left.equalTo(self.passwordTextField.snp.right).offset(8)
+        }
+    }
+}
