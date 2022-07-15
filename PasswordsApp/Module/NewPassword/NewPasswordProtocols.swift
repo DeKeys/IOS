2b9ea93e00681085426adc630d1dadc58d2da1c1
SkullMag <rybalko.oleg.123@mail.ru>
2022-07-16 01:04:19 +0300

    feat(NewPassword): created protocols

--- PasswordsApp/Module/NewPassword/NewPasswordProtocols.swift
+++ PasswordsApp/Module/NewPassword/NewPasswordProtocols.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+// MARK: - Wireframe
+protocol NewPasswordRouterProtocol: AnyObject {
+    func updateCollectionView()
+}
+
+// MARK: - Presenter
+protocol NewPasswordPresenterProtocol: AnyObject {
+    var interactor: NewPasswordInteractorInputProtocol? { get set }
+    
+    func addPassword(serviceName: String, login: String, password: String)
+}
+
+// MARK: - Interactor
+protocol NewPasswordInteractorOutputProtocol: AnyObject {
+    func success()
+    func errorService(message: String)
+}
+
+protocol NewPasswordInteractorInputProtocol: AnyObject {
+    var presenter: NewPasswordInteractorOutputProtocol? { get set }
+    
+    func addPassword(serviceName: String, login: String, password: String)
+}
+
+// MARK: - View
+protocol NewPasswordViewProtocol: AnyObject {
+    var presenter: NewPasswordPresenterProtocol? { get set}
+    
+    func errorService(message: String)
+}
