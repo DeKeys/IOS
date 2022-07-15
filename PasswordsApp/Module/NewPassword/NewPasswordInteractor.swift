ddd01d01145486bcaf861a043d8ac23534302e38
SkullMag <rybalko.oleg.123@mail.ru>
2022-07-16 01:03:56 +0300

    feat: created addPassword function

--- PasswordsApp/Module/NewPassword/NewPasswordInteractor.swift
+++ PasswordsApp/Module/NewPassword/NewPasswordInteractor.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+class NewPasswordInteractor: NewPasswordInteractorInputProtocol {
+    
+    weak var presenter: NewPasswordInteractorOutputProtocol?
+    var coreDataWorker = CoreDataWorker()
+    
+    func addPassword(serviceName: String, login: String, password: String) {
+        // TODO: add password to the server
+        
+        // TODO: change ipfs hash to the returned one from the server
+        let ipfsHash = String(serviceName.hashValue)
+        let password = Password(serviceName: serviceName, login: login, password: password, ipfsHash: ipfsHash, createdAt: Int(Date().timeIntervalSince1970))
+        let encoder = JSONEncoder()
+        do {
+            let encodedPassword = try encoder.encode(password)
+            let status = KeychainWorker.save(key: ipfsHash, data: encodedPassword)
+            if status == errSecSuccess {
+                try coreDataWorker.addPassword(ipfsHash: ipfsHash)
+                presenter?.success()
+            } else {
+                self.presenter?.errorService(message: "Couldn't add password to the keychain. Error \(status)")
+            }
+        } catch {
+            self.presenter?.errorService(message: error.localizedDescription)
+        }
+    }
+    
+}
--- PasswordsApp/Module/NewPassword/NewPasswordPresenter.swift
+++ PasswordsApp/Module/NewPassword/NewPasswordPresenter.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+class NewPasswordPresenter: NewPasswordPresenterProtocol {
+    var interactor: NewPasswordInteractorInputProtocol?
+    var router: NewPasswordRouterProtocol?
+    
+    func addPassword(serviceName: String, login: String, password: String) {
+        interactor?.addPassword(serviceName: serviceName, login: login, password: password)
+    }
+    
+}
+
+extension NewPasswordPresenter: NewPasswordInteractorOutputProtocol {
+    func errorService(message: String) {
+        
+    }
+    
+    func success() {
+        
+    }
+}
