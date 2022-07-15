7ad3df63cde8626ab212a189d3568c72941cf2c5
SkullMag <rybalko.oleg.123@mail.ru>
2022-07-16 01:39:03 +0300

    feat(NewPassword): added functions to the presenter

--- PasswordsApp/Module/NewPassword/NewPasswordPresenter.swift
+++ PasswordsApp/Module/NewPassword/NewPasswordPresenter.swift
@@ -8,21 +8,36 @@
 import Foundation
 
 class NewPasswordPresenter: NewPasswordPresenterProtocol {
+    weak private var view: NewPasswordViewProtocol?
     var interactor: NewPasswordInteractorInputProtocol?
-    var router: NewPasswordRouterProtocol?
+    private let router: NewPasswordRouterProtocol?
+    
+    init(interface: NewPasswordViewProtocol, interactor: NewPasswordInteractorInputProtocol?, router: NewPasswordRouterProtocol) {
+        self.view = interface
+        self.interactor = interactor
+        self.router = router
+    }
     
     func addPassword(serviceName: String, login: String, password: String) {
         interactor?.addPassword(serviceName: serviceName, login: login, password: password)
+    }
+    
+    func generatePassword() {
+        interactor?.generatePassword()
     }
     
 }
 
 extension NewPasswordPresenter: NewPasswordInteractorOutputProtocol {
     func errorService(message: String) {
-        
+        view?.errorService(message: message)
     }
     
     func success() {
-        
+        view?.close()
+    }
+    
+    func generatedPassword(password: String) {
+        view?.setPassword(password: password)
     }
 }
